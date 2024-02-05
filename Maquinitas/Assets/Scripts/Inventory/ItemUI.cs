c9d06a48e2ba69b454fcd240e3e97c793804f6a4
a21hao <94469307+a21hao@users.noreply.github.com>
2024-02-05 18:10:14 +0100

    a

--- Maquinitas/Assets/Scripts/Inventory/ItemUI.cs
+++ Maquinitas/Assets/Scripts/Inventory/ItemUI.cs
@@ -7,8 +7,30 @@ using UnityEngine.EventSystems;
 
 public class ItemUI : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    //[SerializeField] private DataBase db;
-    [SerializeField] private GameObject;
+    [SerializeField] private PlantillaObjetos db;
+    [SerializeField] private GameObject deletteButtom;
+    public int id;
+    public int quantity;
+    [HideInInspector] public PlantillaObjetos.Mercancia itemData;
+    [HideInInspector] public Transform exParent;
+
+    TextMeshProUGUI quantityText;
+    Image iconoImage;
+    Vector3 dragOffset;
+
+    //void Awake()
+    //{
+    //    quantityText = transform.GetComponent<TextMeshProUGUI>();
+    //    iconoImage = GetComponent<Image>();
+
+    //    exParent = transform.parent;
+    //    if (exParent.GetComponent<Image>())
+    //    {
+    //        exParent.GetComponent<Image>().fillCenter = true;
+    //    }
+
+    //    InitializeItem(id, quantity);
+    //}
 
     public void OnBeginDrag(PointerEventData eventData)
     {

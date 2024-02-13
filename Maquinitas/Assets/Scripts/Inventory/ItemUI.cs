0199e29316a2edfa62b423ccaa4d87c373a9d267
a21hao <94469307+a21hao@users.noreply.github.com>
2024-02-13 15:33:06 +0100

    no message

--- Maquinitas/Assets/Scripts/Inventory/ItemUI.cs
+++ Maquinitas/Assets/Scripts/Inventory/ItemUI.cs
@@ -7,50 +7,65 @@ using UnityEngine.EventSystems;
 
 public class ItemUI : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    [SerializeField] private PlantillaObjetos db;
-    [SerializeField] private GameObject deletteButtom;
-    public int id;
-    public int quantity;
-    [HideInInspector] public PlantillaObjetos.Mercancia itemData;
-    [HideInInspector] public Transform exParent;
+    //[SerializeField] private PlantillaObjetos db;
+    //[SerializeField] private GameObject deletteButtom;
+    //public int id;
+    //public int quantity;
+    //[HideInInspector] public PlantillaObjetos.Mercancia itemData;
+    //[HideInInspector] public Transform exParent;
 
-    TextMeshProUGUI quantityText;
-    Image iconoImage;
-    Vector3 dragOffset;
+    //TextMeshProUGUI quantityText;
+    //Image iconoImage;
+    //Vector3 dragOffset;
 
-    void Awake()
-    {
-        quantityText = transform.GetComponent<TextMeshProUGUI>();
-        iconoImage = GetComponent<Image>();
+    //void Awake()
+    //{
+    //    quantityText = transform.GetComponent<TextMeshProUGUI>();
+    //    iconoImage = GetComponent<Image>();
 
-        exParent = transform.parent;
-        if (exParent.GetComponent<Image>())
-        {
-            exParent.GetComponent<Image>().fillCenter = true;
-        }
+    //    exParent = transform.parent;
+    //    if (exParent.GetComponent<Image>())
+    //    {
+    //        exParent.GetComponent<Image>().fillCenter = true;
+    //    }
 
-        InitializeItem(id, quantity);
-    }
+    //    InitializeItem(id, quantity);
+    //}
 
-    void Update()
-    {
-        if(quantityText != null)
-        {
-            quantityText.text = quantity.ToString();
-        }
-    }
+    //void Update()
+    //{
+    //    if(quantityText != null)
+    //    {
+    //        quantityText.text = quantity.ToString();
+    //    }
+    //}
 
-    public void InitializeItem(int id, int quantity)
-    {
-        itemData.ID = id;
-        itemData.acumulable = db.database[id].acumulable;
-        itemData.descripcionObjeto = db.database[id].descripcionObjeto;
-        itemData.imagenObjeto = db.database[id].imagenObjeto;
-        itemData.nameObjeto = db.database[id].nameObjeto;
-        itemData.tipo = db.database[id].tipo;
-        itemData.maxStack = db.database[id].maxStack;
-        itemData.item = db.database[id].item;
-    }
+    //public void InitializeItem(int id, int quantity)
+    //{
+    //    itemData.ID = id;
+    //    itemData.acumulable = db.database[id].acumulable;
+    //    itemData.descripcionObjeto = db.database[id].descripcionObjeto;
+    //    itemData.imagenObjeto = db.database[id].imagenObjeto;
+    //    itemData.nameObjeto = db.database[id].nameObjeto;
+    //    itemData.tipo = db.database[id].tipo;
+    //    itemData.maxStack = db.database[id].maxStack;
+    //    itemData.item = db.database[id].item;
+
+    //    deletteButtom.SetActive(false);
+    //    iconoImage.sprite = itemData.icono;
+
+    //    this.quantity = quantity;
+    //}
+
+    //public void EnableDeletion(bool enable)
+    //{
+    //    deletteButtom.SetActive(enable);
+    //}
+
+    //public void Delete()
+    //{
+
+    //}
 
     public void OnBeginDrag(PointerEventData eventData)
     {

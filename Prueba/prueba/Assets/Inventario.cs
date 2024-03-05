a9c742d04e64c32e12b1bb7015f4d09b20aef40e
Haojie Lin <94469307+a21hao@users.noreply.github.com>
2024-03-05 13:58:03 +0100

    almacen

--- Prueba/prueba/Assets/Inventario.cs
+++ Prueba/prueba/Assets/Inventario.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.EventSystems;
+using UnityEngine.Experimental.Rendering;
 
 public class Inventario : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class Inventario : MonoBehaviour
     public GraphicRaycaster graphRay;
     public Transform canvas;
     public GameObject objetoSeleccionado;
+    public Transform exParent;
 
     private PointerEventData pointerData;
     private List<RaycastResult> raycastResults;
@@ -34,38 +36,68 @@ public class Inventario : MonoBehaviour
             graphRay.Raycast(pointerData, raycastResults);
             if (raycastResults.Count > 0)
             {
-                if (raycastResults[0].gameObject.GetComponent<Item>()) { 
+                if (raycastResults[0].gameObject.GetComponent<Item>())
+                {
                     objetoSeleccionado = raycastResults[0].gameObject;
+                    exParent = objetoSeleccionado.transform.parent.transform;
                     objetoSeleccionado.transform.SetParent(canvas);
                 }
             }
         }
 
-        if(objetoSeleccionado != null)
+        if (objetoSeleccionado != null)
         {
             objetoSeleccionado.GetComponent<RectTransform>().localPosition = CanvasScreen(Input.mousePosition);
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonUp(0))
         {
             pointerData.position = Input.mousePosition;
             raycastResults.Clear();
             graphRay.Raycast(pointerData, raycastResults);
-            if(raycastResults.Count > 0)
-            {
-                foreach(var resultado in raycastResults)
-                {
-                    if (resultado.gameObject.tag == "Slot")
-                    {
-                        objetoSeleccionado.transform.SetParent(resultado.gameObject.transform);
-                        objetoSeleccionado.transform.localPosition = Vector2.zero;
-                    }
-                }
-                objetoSeleccionado = null;
-            }
-            
+
+            objetoSeleccionado.transform.SetParent(exParent);
+
+                if(raycastResults.Count > 0)
+            //    {
+            //        foreach(var resultado in raycastResults)
+            //        {
+            //            if (resultado.gameObject.tag == "Slot")
+            //            {
+            //                if (resultado.gameObject.GetComponentInChildren<Item>() == null)
+            //                {
+            //                    objetoSeleccionado.transform.SetParent(resultado.gameObject.transform);
+            //                    objetoSeleccionado.transform.localPosition = Vector2.zero;
+            //                    exParent = objetoSeleccionado.transform.parent.transform;
+            //                    Debug.Log("Libre");
+            //                }
+            //                else
+            //                {
+            //                    if(resultado.gameObject.GetComponentInChildren<Item>().ID == objetoSeleccionado.GetComponent<Item>().ID){
+            //                        Debug.Log("ID igual");
+            //                        resultado.gameObject.GetComponentInChildren<Item>().cantidad += objetoSeleccionado.GetComponent<Item>().cantidad;
+            //                        Destroy(objetoSeleccionado.gameObject);
+            //                    }
+            //                    else
+            //                    {
+            //                        Debug.Log("ID diferente");
+            //                        objetoSeleccionado.transform.SetParent(exParent.transform);
+            //                        objetoSeleccionado.transform.localPosition = Vector2.zero;
+            //                    }
+            //                }
+            //            }
+            //            else
+            //            {
+            //                Debug.Log("Fuera del inventario");
+            //                objetoSeleccionado.transform.SetParent(exParent.transform);
+            //                objetoSeleccionado.transform.localPosition = Vector2.zero;
+            //            }
+            //        }
+            //        objetoSeleccionado = null;
+            //    }   
+            //}
+            //raycastResults.Clear();
         }
-        raycastResults.Clear();
     }
     //public void GetIndex(int index)
     //{

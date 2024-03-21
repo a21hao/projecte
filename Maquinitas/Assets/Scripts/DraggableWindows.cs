8132a8992eba690f68f130a3b6fbb191aa657c6c
Haojie Lin <94469307+a21hao@users.noreply.github.com>
2024-03-21 18:25:25 +0100

    venatana arrastrable 1

--- Maquinitas/Assets/Scripts/DraggableWindows.cs
+++ Maquinitas/Assets/Scripts/DraggableWindows.cs
@@ -1,33 +1,39 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DraggableWindow : MonoBehaviour
+
+public class DraggableWindow : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
     private bool arrastrando = false;
-    private Vector3 posicionInicial;
-    private Vector3 posicionRatonInicial;
-    public RectTransform parteArrastre; // Asigna la parte superior de la ventana aquí
+    private RectTransform rectTransform;
+    private Vector2 posicionInicialMouse;
 
-    private void Update()
+    void Start()
     {
-        if (arrastrando && Input.GetMouseButton(0))
-        {
-            Vector3 diferencia = (Input.mousePosition - posicionRatonInicial);
-            transform.position = posicionInicial + diferencia;
-        }
+        rectTransform = GetComponent<RectTransform>();
     }
 
-    public void OnMouseDown()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        if (RectTransformUtility.RectangleContainsScreenPoint(parteArrastre, Input.mousePosition))
+        // Verificar si el clic ocurrió en la parte superior de la ventana
+        if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, eventData.position))
         {
-            posicionInicial = transform.position;
-            posicionRatonInicial = Input.mousePosition;
             arrastrando = true;
+            // Guardar la posición inicial del ratón en relación con la ventana
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out posicionInicialMouse);
         }
     }
 
-    public void OnMouseUp()
+    public void OnDrag(PointerEventData eventData)
     {
-        arrastrando = false;
+        if (arrastrando)
+        {
+            Vector2 posicionRaton = eventData.position;
+            // Convertir la posición del ratón a la posición local de la ventana
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform.parent as RectTransform, posicionRaton, eventData.pressEventCamera, out posicionRaton);
+            // Calcular la posición de la ventana sumando la diferencia entre la posición del ratón y la posición inicial del ratón
+            Vector2 nuevaPosicion = posicionRaton - posicionInicialMouse;
+            rectTransform.localPosition = nuevaPosicion;
+        }
     }
 }

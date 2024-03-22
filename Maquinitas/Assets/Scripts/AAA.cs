f0c6c0e6048ab0dc8c60075dd6ecb7423ff945be
Kobsi7 <a20albmonsal@inspedralbes.cat>
2024-03-22 20:31:27 +0100

    merge completo

--- Maquinitas/Assets/Scripts/AAA.cs
+++ Maquinitas/Assets/Scripts/AAA.cs
@@ -6,61 +6,41 @@ using TMPro;
 public class AAA : MonoBehaviour
 {
 
-    [SerializeField] int money;
-    [SerializeField] int stock;
-    [SerializeField] int maxStock = 10;
+    public TMP_Text moneyUI;
 
-    public MoneyManager moneyManager;
+    [SerializeField]
+    int money;
 
     [SerializeField]
     int maxStock = 10;
-    public List<GameObject> objectList = new List<GameObject>();
 
-    // M�todo que se llama cuando un objeto es arrastrado y soltado sobre el cubo
-    public void OnDropItem(GameObject item)
+    public ObjectBase item1;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        Item itemComponent = item.GetComponent<Item>(); // Obtener componente Item del objeto arrastrado
+        item1.stock = maxStock;
+    }
 
-        if (itemComponent != null)
-        {
-            // Verificar si ya existe un objeto similar en la lista
-            bool found = false;
-            foreach (GameObject obj in objectList)
-            {
-                Item objItem = obj.GetComponent<Item>();
-                if (objItem != null && objItem.GetID() == itemComponent.GetID())
-                {
-                    objItem.SetCantidad(objItem.GetCantidad() + itemComponent.GetCantidad()); // Actualizar cantidad
-                    found = true;
-                    break;
-                }
-            }
+    // Update is called once per frame
+    void Update()
+    {
 
-            // Si no se encontr� un objeto similar, agregarlo a la lista
-            if (!found)
-            {
-                GameObject newItem = Instantiate(item); // Clonar el objeto
-                newItem.transform.SetParent(transform); // Establecer como hijo del cubo
-                newItem.transform.localPosition = Vector3.zero; // Colocar en el centro del cubo
-                objectList.Add(newItem); // Agregar a la lista
-                Debug.Log("Objeto a�adido al cubo: " + newItem.name);
-            }
-
-            // Eliminar el objeto del inventario original
-            Destroy(item);
-        }
     }
 
     public void Buy()
     {
         Debug.Log("i");
         if (item1.stock > 0)
-        {          
+        {
+            
             item1.stock -= 1;
             money += item1.precio;
             Debug.Log(item1.precio);
             //moneyUI.text = money.ToString();
-            MoneyManager.IncrementarDinero(item1.precio);
+            MoneyManager.IncrementarDinero(item1.precio);           
 
+        }
     }
+
 }

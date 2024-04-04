21f82b077852c819c21b4a8f9e6b5f31ccdbb34d
Kobsi7 <a20albmonsal@inspedralbes.cat>
2024-04-04 18:19:03 +0200

    Revert "merge main to vistadia"
    This reverts commit bc05f4e0ba29a643c2b0a2a802eb5aa0a011a6b2, reversing
    changes made to 5e85e850e8c3676af4eedaf3dc8bbe1b16c59b3a.

--- Maquinitas/Assets/Scripts/Spawner.cs
+++ Maquinitas/Assets/Scripts/Spawner.cs
@@ -13,7 +13,7 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spawnTime = 5f;
+        spawnTime = 1f;
         timer = 0;
     }
 

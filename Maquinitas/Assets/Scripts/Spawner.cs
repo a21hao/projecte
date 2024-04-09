44f9e754a9e328f0eb141df264cb430aecfe2e46
A22joesalbio <156098130+A22joesalbio@users.noreply.github.com>
2024-04-09 17:19:23 +0200

    AAAAAA

--- Maquinitas/Assets/Scripts/Spawner.cs
+++ Maquinitas/Assets/Scripts/Spawner.cs
@@ -26,6 +26,7 @@ public class Spawner : MonoBehaviour
         {
             timer = 0;
             Instantiate(prefab, transform.position, Quaternion.identity);
+            spawnTime = Random.Range(3f, 8f);
         }
     }
 }
